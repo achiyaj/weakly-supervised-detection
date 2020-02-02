35d053eddc17cb07c3c7a31148303193c54689e1
Achiya-Jerbi <achiyaj@gmail.com>
2020-02-02 14:17:29 +0200

    fixed bug

--- utils/multiloader.py
+++ utils/multiloader.py
@@ -8,6 +8,7 @@ class MultiLoader:
 
     def __iter__(self):
         self.datasets = [iter(dataset_getter()) for dataset_getter in self.dataset_getters]
+        self.dataset_lengths = [len(self.datasets[i]) for i in range(len(self.datasets))]
         self.times_to_sample_left = self.sampling_ratios.copy()
         self.num_objs_left = [len(self.datasets[i]) * self.sampling_ratios[i] for i in range(len(self.datasets))]
         return self
@@ -21,12 +22,14 @@ class MultiLoader:
         if sum(self.num_objs_left) == 0:
             raise StopIteration
         dset_idx = choices(range(len(self.datasets)), self.normalize_to_dist(self.num_objs_left))[0]
+        cur_batch = next(self.datasets[dset_idx])
         self.num_objs_left[dset_idx] -= 1
-        if self.num_objs_left[dset_idx] % len(self.datasets[dset_idx]) and self.times_to_sample_left[dset_idx] > 0:
+        if (self.num_objs_left[dset_idx] % self.dataset_lengths[dset_idx] == 0) and \
+                self.times_to_sample_left[dset_idx] > 0:
             self.datasets[dset_idx] = iter(self.dataset_getters[dset_idx]())
             self.times_to_sample_left[dset_idx] -= 1
 
-        return next(self.datasets[dset_idx])
+        return cur_batch
 
     def __len__(self):
         return sum([len(self.datasets[i]) * self.sampling_ratios[i] for i in range(len(self.datasets))])
